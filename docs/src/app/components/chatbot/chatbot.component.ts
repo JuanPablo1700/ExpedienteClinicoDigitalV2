247cfa89ffedb5a459bda97e2c0da33b7d01de48
ArletheMora <armomorali@ittepic.edu.mx>
2022-05-21 15:59:00 -0600

    mostrar mensajes en chat

--- docs/src/app/components/chatbot/chatbot.component.ts
+++ docs/src/app/components/chatbot/chatbot.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-chatbot',
@@ -6,10 +7,39 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./chatbot.component.css']
 })
 export class ChatbotComponent implements OnInit {
+  persona: any;
+  nuevoMensaje: string = "";
+
+  mensajes: any = [
+  {
+    emisor: "id",
+    texto: "Hola que tal"
+  }, {
+    emisor: "id",
+    texto: "Todo bien y tú?"
+  }, {
+    emisor: "id",
+    texto: "Todo perfecto"
+  }, {
+    emisor: "id",
+    texto: "Hola a todos"
+  }, {
+    emisor: "id",
+    texto: "Hola 2"
+  },
+  {
+    emisor: "id",
+    texto: "Hola 3"
+  }]
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  enviarMensaje(){
+    console.log(this.nuevoMensaje);
+    this.nuevoMensaje = "";
   }
 
 }
