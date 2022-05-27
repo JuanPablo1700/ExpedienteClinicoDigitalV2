b918b8c880e7283e0bedf130d5ef1cca9a5c4106
Juan Campos <jcamposcasillas@gmail.com>
2022-05-27 17:39:25 -0600

    ChatBot con django

---
+++ chatbot/polls/views.py
@@ -0,0 +1,9 @@
+from urllib import response
+from django.http import HttpResponse
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+
+def index(request):
+    return render(request, "chatbot/index.html")
